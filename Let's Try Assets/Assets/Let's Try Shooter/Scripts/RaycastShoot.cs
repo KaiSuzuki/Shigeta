94352099b5e2369d18a47c67deb877afb5048317
KaiSuzuki <ktiars64@gmail.com>
2018-03-04 01:14:53 +0700

    GunShooterスクリプト編集1

---
+++ Let's Try Assets/Assets/Let's Try Shooter/Scripts/RaycastShoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShoot : MonoBehaviour {
+
+    public int gunDamage = 1;
+    public float fireRate = .25f;
+    public float weponRange = 50f;
+    public float hitForce = 100f;
+    public Transform gunEnd;
+
+
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+    private AudioSource gunAudio;
+    private LineRenderer laserline;
+    private float nextFire;
+    
+
+	void Start () {
+        laserline = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        fpsCam = GetComponentInParent<Camera>();
+	}
+	
+	void Update () {
+		if(Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+        }
+	}
+}
